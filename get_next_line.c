44dc9206b5e87c61735a66f9d98cdd6f03671f70
Hyeongseop Shin <hyeongsh@c2r5s5.42seoul.kr>
2023-10-13 16:22:51 +0900

    2

--- get_next_line.c
+++ get_next_line.c
@@ -6,57 +6,11 @@
 /*   By: hyeongsh <hyeongsh@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 13:35:34 by hyeongsh          #+#    #+#             */
-/*   Updated: 2023/10/13 15:01:17 by hyeongsh         ###   ########.fr       */
+/*   Updated: 2023/10/13 16:22:33 by hyeongsh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
-#include <fcntl.h>
-
-int	main(int ac, char **av)
-{
-	char	*a;
-	int		fd = open(av[1], O_RDONLY);
-	
-	ac++;
-	a = av[1];
-	while (a != 0)
-	{
-		a = get_next_line(fd);
-		printf("a in main: [%s]\n", a);
-	}
-}
-
-char	*ft_strncpy(char *dst, char *src, int n)
-{
-	int	count;
-
-	if (dst == 0 && src == 0)
-		return (dst);
-	count = 0;
-	while (count < n && src[count] != 0)
-	{
-		dst[count] = src[count];
-		count++;
-	}
-	dst[count] = 0;
-	return (dst);
-}
-
-int	ft_len(char *str)
-{
-	int	size;
-
-	if (str == 0)
-		return (0);
-	size = 0;
-	while (str[size] != 0 && str[size] != '\n')
-		size++;
-	if (str[size] == '\n')
-		size++;
-	return (size);
-}
 
 char	*ft_strjoin(char *receive, char *buf, char *leave)
 {
@@ -83,21 +37,11 @@ char	*ft_strjoin(char *receive, char *buf, char *leave)
 	return (toss);
 }
 
-int	ft_read(int fd, char *buf)
-{
-	int i;
-
-	i = -1;
-	while (++i < BUFFER_SIZE + 1)
-		buf[i] = 0;
-	return (read(fd, buf, BUFFER_SIZE));
-}
-
 char	*get_next_line(int fd)
 {
 	char		*toss;
 	char		buf[BUFFER_SIZE + 1];
-	static char	leave[BUFFER_SIZE + 1] = {0, };
+	static char	leave[BUFFER_SIZE + 1];
 
 	if (BUFFER_SIZE == 0)
 		return (0);
@@ -105,24 +49,16 @@ char	*get_next_line(int fd)
 	toss = ft_strjoin(toss, leave, leave);
 	while (*leave == 0 && ft_check(toss))
 	{
-		if (ft_read(fd, buf) == 0)
+		if (ft_read(fd, buf) <= 0)
 		{
 			if (*toss == 0)
+			{
+				free(toss);
 				return (0);
+			}
 			return (toss);
 		}
 		toss = ft_strjoin(toss, buf, leave);
 	}
 	return (toss);
-}
-
-int	ft_check(char *toss)
-{
-	while (*toss)
-	{
-		if (*toss == '\n')
-			return (0);
-		toss++;
-	}
-	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: hyeongsh <hyeongsh@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 13:36:51 by hyeongsh          #+#    #+#             */
-/*   Updated: 2023/10/13 15:01:15 by hyeongsh         ###   ########.fr       */
+/*   Updated: 2023/10/13 16:20:21 by hyeongsh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,11 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10
+#  define BUFFER_SIZE 101
 # endif
 
 # include <stdlib.h>
 # include <unistd.h>
-
-typedef struct s_list
-{
-	int			fd;
-	char		leave[BUFFER_SIZE + 1];
-	struct s_list	*next;
-}	t_list;
 
 char	*ft_strjoin(char *receive, char *buf, char *leave);
 char	*get_next_line(int fd);
