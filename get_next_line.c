f4d60bd76010c47c2773c701af9b03583429620a
Hyeongseop Shin <hyeongsh@c2r4s6.42seoul.kr>
2023-10-16 23:41:28 +0900

    5

--- get_next_line.c
+++ get_next_line.c
@@ -5,62 +5,152 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyeongsh <hyeongsh@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/10 13:35:34 by hyeongsh          #+#    #+#             */
-/*   Updated: 2023/10/15 14:06:28 by hyeongsh         ###   ########.fr       */
+/*   Created: 2023/10/16 19:13:56 by hyeongsh          #+#    #+#             */
+/*   Updated: 2023/10/16 23:40:01 by hyeongsh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+/*
+#include <stdio.h>
+#include <fcntl.h>
 
-char	*ft_strjoin(char *receive, char *buf, char *leave)
+int	main(void)
 {
-	char	*toss;
-	int		i;
-	int		j;
-	int		len;
+	int	fd1;
 
-	toss = (char *)malloc(ft_len(receive) + ft_len(buf) + 1);
-	i = -1;
-	len = ft_len(receive);
-	while (++i < len)
-		toss[i] = receive[i];
-	j = -1;
-	len = ft_len(buf);
-	while (++j < len)
-		toss[i + j] = buf[j];
-	toss[i + j] = 0;
-	if (buf[j] != 0)
-		leave = ft_strncpy(leave, &buf[j], BUFFER_SIZE - j);
-	else
-		*leave = 0;
-	free(receive);
-	return (toss);
-}
+	fd1 = open("test/qqq.txt", O_RDONLY);
+	printf("/%s", get_next_line(fd1));
+	printf("/%s", get_next_line(fd1));
+	printf("/%s", get_next_line(fd1));
+	printf("/%s", get_next_line(fd1));
+	printf("/%s", get_next_line(fd1));
+	printf("/%s", get_next_line(fd1));
+	return (0);
+}*/
 
 char	*get_next_line(int fd)
 {
-	char		*toss;
-	char		buf[BUFFER_SIZE + 1];
-	static char	leave[BUFFER_SIZE + 1];
-	int			readsize;
+	static t_list	*head;
+	char			*toss;
+	t_list			*cur;
 
-	if (BUFFER_SIZE == 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
-	toss = 0;
-	toss = ft_strjoin(toss, leave, leave);
-	while (*leave == 0 && ft_check(toss))
+	cur = ft_lstadd_back(&head, fd);
+	if (cur == 0)
+		return (0);
+	toss = ft_setstr(cur->leave, cur);
+	if (cur->leave[0] == 0 && ft_check(toss))
 	{
-		readsize = ft_read(fd, buf);
+		toss = ft_strjoin(toss, fd, BUFFER_SIZE);
+		toss = ft_substr(toss, cur, 0);
+	}
+	if (toss == 0)
+		ft_lstdel(&head, fd);
+	return (toss);
+}
+
+t_list	*ft_lstadd_back(t_list **head, int fd)
+{
+	t_list	*cur;
+	t_list	*tmp;
+
+	cur = *head;
+	while (cur != NULL)
+	{
+		tmp = cur;
+		if (cur->fd == fd)
+			return (cur);
+		cur = cur->next;
+	}
+	cur = (t_list *)malloc(sizeof(t_list));
+	if (cur == 0)
+		return (0);
+	cur->fd = fd;
+	cur->next = NULL;
+	cur->leave[0] = 0;
+	if (*head == 0)
+		*head = cur;
+	else
+		tmp->next = cur;
+	return (cur);
+}
+
+char	*ft_setstr(char	*leave, t_list *cur)
+{
+	char	*toss;
+	int		i;
+
+	toss = (char *)malloc(BUFFER_SIZE + 1);
+	if (toss == 0)
+		return (0);
+	i = 0;
+	while (*leave != 0 && *leave != '\n')
+		toss[i++] = *leave++;
+	if (*leave == '\n')
+		toss[i++] = *leave++;
+	toss[i] = 0;
+	i = 0;
+	while (*leave != 0)
+		cur->leave[i++] = *leave++;
+	cur->leave[i] = 0;
+	return (toss);
+}
+
+char	*ft_strjoin(char *toss, int fd, int size)
+{
+	char	buf[BUFFER_SIZE + 1];
+	int		readsize;
+	int		count;
+
+	count = 0;
+	while (++count)
+	{
+		readsize = read(fd, buf, BUFFER_SIZE);
 		if (readsize <= 0)
 		{
-			if (*toss == 0 || readsize < 0)
+			if (toss[0] == 0 || readsize < 0)
 			{
 				free(toss);
-				return (0);
+				toss = 0;
 			}
 			return (toss);
 		}
-		toss = ft_strjoin(toss, buf, leave);
+		buf[readsize] = 0;
+		if (size <= BUFFER_SIZE * count)
+			toss = ft_newstr(toss, &size);
+		if (ft_strcat(toss, buf) == 1)
+			return (toss);
 	}
-	return (toss);
+	return (0);
+}
+
+char	*ft_substr(char *toss, t_list *cur, int len)
+{
+	char	*compact;
+	int		i;
+
+	if (toss == 0)
+		return (0);
+	while (toss[len] != 0 && toss[len] != '\n')
+		len++;
+	if (toss[len] == '\n')
+		len++;
+	compact = (char *)malloc(len + 1);
+	if (compact == 0)
+	{
+		free(toss);
+		return (0);
+	}
+	i = -1;
+	while (++i < len)
+		compact[i] = toss[i];
+	compact[i] = 0;
+	i = 0;
+	while (toss[len] != 0)
+		cur->leave[i++] = toss[len++];
+	cur->leave[i] = 0;
+	free(toss);
+	return (compact);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyeongsh <hyeongsh@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/10 13:36:51 by hyeongsh          #+#    #+#             */
-/*   Updated: 2023/10/13 16:20:21 by hyeongsh         ###   ########.fr       */
+/*   Created: 2023/10/16 19:21:48 by hyeongsh          #+#    #+#             */
+/*   Updated: 2023/10/16 23:36:50 by hyeongsh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,27 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 101
+#  define BUFFER_SIZE 10
 # endif
 
 # include <stdlib.h>
 # include <unistd.h>
 
-char	*ft_strjoin(char *receive, char *buf, char *leave);
+typedef struct s_list
+{
+	int				fd;
+	char			leave[BUFFER_SIZE + 1];
+	struct s_list	*next;
+}	t_list;
+
 char	*get_next_line(int fd);
-int		ft_len(char *str);
-char	*ft_strncpy(char *dst, char *src, int n);
-int		ft_read(int fd, char *buf);
+t_list	*ft_lstadd_back(t_list **head, int fd);
+char	*ft_setstr(char	*leave, t_list *cur);
+char	*ft_strjoin(char *toss, int fd, int size);
+char	*ft_newstr(char *toss, int *size);
+int		ft_strcat(char *toss, char *buf);
+char	*ft_substr(char *toss, t_list *cur, int len);
 int		ft_check(char *toss);
+void	ft_lstdel(t_list **head, int fd);
 
 #endif
